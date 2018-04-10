e48b59b0c172d400d6e28747a7fe80051df16988
Jules Petit <jules.laptop@gmail.com>
2018-04-10 17:49:49 -0400

    Doesnt want to autoscroll

--- src/com/XCoin/GUI/MinerGui.java
+++ src/com/XCoin/GUI/MinerGui.java
@@ -1,10 +1,12 @@
 package com.XCoin.GUI;
 //Imports
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
@@ -17,6 +19,8 @@ public class MinerGui extends JFrame implements ActionListener{
 	private JButton startMine;
 	private JButton stopMine;
 	private JTextArea loggerOutput;
+	private JPanel middlePanel;
+	private JScrollPane pane;
 
 	/**
 	 * Default Constructor to start the gui program
@@ -29,26 +33,18 @@ public class MinerGui extends JFrame implements ActionListener{
 		constraints.fill = GridBagConstraints.HORIZONTAL; // Horizontal Layout
 
 		loggerOutput = new JTextArea(); //Set up the output log
-		constraints.gridx = 0; //Center
-		constraints.gridy = 0; //Center
-		constraints.gridwidth = 1;
 		loggerOutput.setEditable(false); // No Editing output
-		panel.add(loggerOutput, constraints); // Add it to the panel
+		loggerOutput.setPreferredSize(new Dimension(400,400));
+		pane = new JScrollPane(loggerOutput);
+		pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		pane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		panel.add(pane);
 
 		startMine = new JButton("Start Miner"); // Create new button for starting miner program
-		//constraints.gridx = 0; 
-		//constraints.gridy = 1;
-		///constraints.gridwidth = 2;
-		//panel.add(startMine, constraints); // Add with coordinates
 		startMine.addActionListener(this); //Adds a listener for a click of the button
 		
 		stopMine = new JButton("Stop Miner");// Create new button for stopping miner program
-		////constraints.gridx = 0;
-		//constraints.gridy = 2;
-		//constraints.gridwidth = 2;
-		//panel.add(stopMine, constraints);// Add with coordinates
 		stopMine.addActionListener(this);//Adds a listener for a click of the button
-		
 		
 		JPanel button = new JPanel();
 		button.add(startMine);
@@ -63,11 +59,25 @@ public class MinerGui extends JFrame implements ActionListener{
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent e){
 		if(e.getSource().equals(startMine)) {
 			System.out.println("Start Mining");
+			BufferedReader f = null;
+			try {
+				f = new BufferedReader(new FileReader("MinerTestFile.in"));
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			}                                   
+			Scanner input = new Scanner(f);
+			while(input.hasNextLine()) {
+				String output = input.nextLine();
+				loggerOutput.append(output + "\n");
+				loggerOutput.setCaretPosition(loggerOutput.getDocument().getLength());
+				this.setLayout(new FlowLayout());
+			}
 		}else {
 			System.out.println("Stop Mining");
+			loggerOutput.setText("");
 		}
 	}
 }
