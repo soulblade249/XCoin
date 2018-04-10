e3331a6617ad0727d10d0b8dc05bc581809c81e6
Jules Petit <jules.laptop@gmail.com>
2018-04-10 14:18:41 -0400

    Added a gui. Yet to test

--- src/com/XCoin/GUI/MinerGui.java
+++ src/com/XCoin/GUI/MinerGui.java
@@ -2,6 +2,7 @@ package com.XCoin.GUI;
 //Imports
 import javax.swing.*;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -12,46 +13,51 @@ import java.io.*;
 
 public class MinerGui extends JFrame implements ActionListener{
 
-	private JButton startMine = new JButton("Start Miner");
-	private JButton stopMine = new JButton("Stop Miner");
-	private JTextArea  loggerOutput = new JTextArea();
-	private JPanel pane = null;
-	private GridBagConstraints cs = null;
+	private JButton startMine;
+	private JButton stopMine;
+	private JTextArea loggerOutput;
 
 	/**
 	 * Default Constructor to start the gui program
 	 */
 	public MinerGui() {
 		super("XCoin Miner G.U.I"); // Calls the JFrame class to create a new frame with title
-		pane = new JPanel(new GridBagLayout());
-		cs.fill = GridBagConstraints.HORIZONTAL;
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); // Creates a dimension tool to center the frame on the screen
-		setSize(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2); //Should Center Window
-		pane.add(startMine);
-		pane.add(stopMine);
-		getContentPane().add(pane);
-		loggerOutput.setEditable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
+		JPanel panel = new JPanel(new GridBagLayout()); // Creates  a new panel with a layout
+		GridBagConstraints constraints = new GridBagConstraints(); //Creates the layout
 
-	/**
-	 * This method appends the data to the text area.
-	 * @param data the miner prints out
-	 */
-	public void showInfo(String data) {
-		loggerOutput.append(data);
-		this.getContentPane().validate();
+		constraints.fill = GridBagConstraints.HORIZONTAL; // Horizontal Layout
+
+		loggerOutput = new JTextArea(); //Set up the output log
+		constraints.gridx = 0; //Center
+		constraints.gridy = 0; //Center
+		constraints.gridwidth = 1;
+		panel.add(loggerOutput, constraints); // Add it to the panel
+
+		startMine = new JButton("Start Miner"); // Create new button for starting miner program
+		constraints.gridx = 0; 
+		constraints.gridy = 1;
+		constraints.gridwidth = 2;
+		panel.add(startMine, constraints); // Add with cordinates
+		startMine.addActionListener(this); //Adds a listener for a click of the button
+		
+		stopMine = new JButton("Stop Miner");// Create new button for stopping miner program
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		constraints.gridwidth = 2;
+		panel.add(stopMine, constraints);// Add with cordinates
+		stopMine.addActionListener(this);//Adds a listener for a click of the button
+
+		pack(); // Pack the frame
+		setResizable(false);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	public static void main(String[] args) {
-		MinerGui gui = new MinerGui();
+		if(e.getSource().equals(startMine)) {
+			System.out.println("Start Mining");
+		}else {
+			System.out.println("Stop Mining");
+		}
 	}
 }
 
