dbdefdb039d6b7fb615632308b7daf80eea909ae
Jules Petit <jules.laptop@gmail.com>
2018-05-02 11:15:27 -0400

    Leading BYtes

--- src/com/XCoin/Core/Transaction.java
+++ src/com/XCoin/Core/Transaction.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.ByteBuffer;
 import java.security.Key;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -19,10 +20,10 @@ public class Transaction {
 	private final byte[] nonce;
 	private final byte[] sender;
 	private final byte[] receiver;
-	private final byte[] signature;
+	private final  byte[] signature;
 	private final byte[] networkId;
 	/* First byte is amount of different currency type, leading bytes will be currencies involved(firstByte(currencyType), secondByte(amount of bytes needed to hold the amount), thirdByte(amount)). */
-	private final byte[] data;
+	private static byte[] data;
 	
 	public Transaction(byte[] nonce, byte[] sender, byte[] receiver, byte[] signature, byte[] networkid, byte[] data) {
 		this.data = data;
@@ -38,15 +39,24 @@ public class Transaction {
 		return HashUtil.applySHA256(ByteUtil.concat(nonce, sender, receiver, signature, networkId));
 	}
 	
-	private byte getLeadingByte() {
+	private static Byte getLeadingByte() {
 		return data[0];
 	}
 	
+	private static byte getBytes() {
+		return 0;
+	}
+	
 	public static void main(String args[]) {
-		byte[] data = "1".getBytes();
-		Transaction test = new Transaction("test".getBytes(), "jules".getBytes(), "steven".getBytes(), "ghy".getBytes(), "main".getBytes(), "123".getBytes());
-		String output = new String(new byte[] {test.getLeadingByte()});
-		System.out.println("Leading Byte: " + output);
+		int test2 = 136;
+		String te = "test";
+		byte[] dat = {(byte)test2};
+		byte[] dat2 = te.getBytes();
+		byte[] realDat = ByteUtil.merge(dat, dat2);
+		Transaction test = new Transaction("test".getBytes(), "jules".getBytes(), "steven".getBytes(), "ghy".getBytes(), "main".getBytes(), realDat);
+		//int i = getLeadingByte().intValue() & 0xFF; -- This code gets you the unisgned value of the first element of data
+		//System.out.println("Leading Number: " + i);
+		//System.out.println("Real Data Array: " + realDa);
 	}
 	
 }
