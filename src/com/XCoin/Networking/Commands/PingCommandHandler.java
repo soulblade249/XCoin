c360fb66a00a7b53bc813cd39f93cd1df561c7c9
Steven Rossi <rossisteven67@gmail.com>
2018-06-06 09:17:13 -0400

    Updating commands

--- src/com/XCoin/Networking/Commands/PingCommandHandler.java
+++ src/com/XCoin/Networking/Commands/PingCommandHandler.java
@@ -10,19 +10,19 @@ public class PingCommandHandler extends Command {
 
 	private BigInteger bInt;
 	
-	public byte[] recieve(ByteArrayKey args) {
-		System.out.println(args.toString());
-		return null;
+	public byte[] recieve(byte[] data) {
+		System.out.println(ByteUtil.bytesToBigInteger(data));
+		return ByteUtil.concat(new ByteArrayKey((byte) 0xFF).toByteArray(), new ByteArrayKey((byte) 0x01).toByteArray(), data);
 	}
 
-	public byte[] send(ByteArrayKey args) {
+	public byte[] send(byte[] data) {
 		bInt = BigInteger.valueOf(new Random().nextLong());
 		System.out.println("Sending Int: " + bInt.toString() );
 		System.out.println(ByteUtil.bigIntegerToBytes(bInt).length);
 		return ByteUtil.concat(new ByteArrayKey((byte) 0xFF).toByteArray(), new ByteArrayKey((byte) 0x01).toByteArray(), ByteUtil.bigIntegerToBytes(bInt));
 	}
 	
-	private byte[] check(ByteArrayKey args) {
+	private byte[] check(byte[] data) {
 		return null;
 	}
 
@@ -31,12 +31,12 @@ public class PingCommandHandler extends Command {
 		System.out.println("Test");
 		if(new ByteArrayKey(args.toByteArray()[1]).equals(new ByteArrayKey((byte) 0x00))) {
 			System.out.println("Sending Ping");
-			return send(args); 
+			return send(args.subSet(2, args.toByteArray().length)); 
 		} else if(new ByteArrayKey(args.toByteArray()[1]).equals(new ByteArrayKey((byte) 0x01))) {
 			System.out.println("Recieved Ping");
-			return recieve(args);
+			return recieve(args.subSet(2, args.toByteArray().length));
 		} else if(new ByteArrayKey(args.toByteArray()[1]).equals(new ByteArrayKey((byte) 0x02))) {
-			return check(args);
+			//return check(args);
 		}
 	
 		return null;
